b845a2f509e5406a95929d9c816c6637bf154fad
Rukmini-Meda <rukmini.meda@gmail.com>
2021-11-27 15:23:51 +0530

    Added env files

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,4 +1,10 @@
+const {
+    MONGODB_ATLAS_DATABASE,
+    MONGODB_ATLAS_PASSWORD,
+    MONGODB_ATLAS_USER
+} = process.env;
+
 module.exports = {
-    mongoURI: "mongodb+srv://acadboxuser:toolboxforacademics@acadbox-cluster.bhnu6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    mongoURI: `mongodb+srv://${MONGODB_ATLAS_USER}:${MONGODB_ATLAS_PASSWORD}@acadbox-cluster.bhnu6.mongodb.net/${MONGODB_ATLAS_DATABASE}?retryWrites=true&w=majority`,
     secretOrKey: "secret"
 }
